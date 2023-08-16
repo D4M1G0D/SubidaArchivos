7ba89c39d7b345acef8c8b04c8580c8dfe94338f
D4M1GOD <damian20002010@live.com>
2023-08-16 18:48:20 -0500

    1.0

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const cors = require('cors');
+
+const app = express()
+
+app.use(fileUpload())
+app.use(cors());
+
+app.post('/upload',(req,res) => {
+    let EDFile = req.files.file
+
+    EDFile.mv(`./files/${EDFile.name}`,err => {
+        if(err) return res.status(500).send({ message : err })
+        return res.status(200).send({ message : 'File upload' })
+
+    })
+
+})
+
+app.listen(3000,() => console.log('Corriendo'))
